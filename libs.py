61d95a000f2c24602bdf49de7c6e8c8d8876ade5
kukuhpradipto <kukuhpradipto@gmail.com>
2025-05-18 11:18:33 +0700

    penambahan materi function

---
+++ libs.py
@@ -0,0 +1,8 @@
+
+def welcome_message () : 
+    
+    welcome = "SELAMAT DATANG DI GAMES"
+    style = "*" * (len(welcome) + 6)  
+    print(f"{style}")
+    print(f"** {welcome} **")
+    print(f"{style}")
--- test.py
+++
@@ -1,8 +0,0 @@
-import random
-
-posisi_kelinci = random.randint(1,4)
-inputUser = input("input !...")
-
-while int(inputUser) not in (1,2,3,4) :
-    inputUser = input("input yang betul...")
-print("betul")
